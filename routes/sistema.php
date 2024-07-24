27c4163aae6db6bd88f7dc5d76ca8f69f7252910
Gabriel Oliveira de Souza <gabriel@umobi.com.br>
2024-07-24 11:54:16 -0300

    feat: Adicionar rota de login e redirecionamento após o login

--- routes/sistema.php
+++ routes/sistema.php
@@ -5,11 +5,15 @@ use App\Http\Controllers\Web\VistoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout.header');
+    return redirect()->route('login');
 });
 
 Route::get('/login', function () {
     return view('auth.login');
+})->name('login');
+
+Route::post('/login', function () {
+    return redirect()->route('arts.index');
 });
 
 
